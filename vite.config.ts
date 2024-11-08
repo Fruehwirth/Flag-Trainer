0e5eb14d0123045c57abecae5cd3e5c237d02fca
Michael Frühwirth <32189690+Fruehwirth@users.noreply.github.com>
2024-11-08 22:22:10 +0100

    Fixed to run locally again

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  base: '/flag-trainer/',
+  base: process.env.NODE_ENV === 'production' ? '/flag-trainer/' : '/',
   plugins: [react()],
   server: {
     port: 3000,
