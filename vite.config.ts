4e44c08137fb13a7f28f26e12d065c2c14c3db84
Michael Frühwirth <32189690+Fruehwirth@users.noreply.github.com>
2024-11-09 01:45:07 +0100

    wow it works

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,26 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
 
 export default defineConfig(({ command }) => ({
   plugins: [react()],
   base: command === 'serve' ? '' : 'https://fruehwirth.github.io/Flag-Trainer/',
   build: {
     outDir: 'dist',
-    assetsDir: 'assets'
-  }
+    assetsDir: 'assets',
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, 'index.html'),
+      },
+      output: {
+        assetFileNames: (assetInfo) => {
+          if (assetInfo.name.endsWith('.json')) {
+            return 'assets/translations/[name][extname]'
+          }
+          return 'assets/[name]-[hash][extname]'
+        }
+      }
+    }
+  },
+  publicDir: 'public'
 }))
