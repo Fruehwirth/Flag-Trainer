76b9f3bdb2f29850ee537f5bfad7885f3f483e10
Michael Frühwirth <32189690+Fruehwirth@users.noreply.github.com>
2024-11-09 01:40:46 +0100

    sdfdsfsdfsdfsdfsdf

--- vite.config.ts
+++ vite.config.ts
@@ -1,22 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
-import fs from 'fs-extra'
 
-export default defineConfig({
-  base: '/flag-trainer/',
-  plugins: [
-    react(),
-    {
-      name: 'copy-files',
-      closeBundle: async () => {
-        await fs.copy('public/404.html', 'dist/404.html')
-        await fs.copy('public/200.html', 'dist/200.html')
-      }
-    }
-  ],
+export default defineConfig(({ command }) => ({
+  plugins: [react()],
+  base: command === 'serve' ? '' : 'https://fruehwirth.github.io/Flag-Trainer/',
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   }
-})
+}))
