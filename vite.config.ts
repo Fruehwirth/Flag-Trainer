df4dc175815f1315988cdbc1d7127b1bfb017b97
Michael Frühwirth <32189690+Fruehwirth@users.noreply.github.com>
2024-11-09 01:10:08 +0100

    change

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 import { resolve } from 'path'
 
 export default defineConfig(({ command }) => ({
-  base: '/flag-trainer/',
+  base: command === 'serve' ? '/' : '/flag-trainer/',
   plugins: [react()],
   server: {
     port: 3000,
