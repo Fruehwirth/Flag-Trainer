fe37e1ee4c15252b6c8bb3494b921c8e7dd5d2c9
Michael Frühwirth <32189690+Fruehwirth@users.noreply.github.com>
2024-11-09 01:34:51 +0100

    holadaro

--- vite.config.ts
+++ vite.config.ts
@@ -1,30 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import { resolve } from 'path'
 
 export default defineConfig(({ command }) => ({
-  base: '/flag-trainer/',
+  base: command === 'serve' ? '' : '/flag-trainer/',
   plugins: [react()],
-  server: {
-    port: 3000,
-    open: true
-  },
   build: {
-    sourcemap: true,
     outDir: 'dist',
-    assetsDir: 'assets',
-    rollupOptions: {
-      output: {
-        manualChunks: undefined,
-        entryFileNames: 'assets/[name].[hash].js',
-        chunkFileNames: 'assets/[name].[hash].js',
-        assetFileNames: 'assets/[name].[hash].[ext]'
-      }
-    }
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, 'src'),
-    },
+    assetsDir: 'assets'
   }
 }))
