f27941435a2781a7471b117e30bf2baa56e9f25d
Michael Frühwirth <32189690+Fruehwirth@users.noreply.github.com>
2024-11-09 01:37:53 +0100

    added custom 404

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,22 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { resolve } from 'path'
+import fs from 'fs-extra'
 
-export default defineConfig(({ command }) => ({
-  base: command === 'serve' ? '' : '/flag-trainer/',
-  plugins: [react()],
+export default defineConfig({
+  base: '/flag-trainer/',
+  plugins: [
+    react(),
+    {
+      name: 'copy-files',
+      closeBundle: async () => {
+        await fs.copy('public/404.html', 'dist/404.html')
+        await fs.copy('public/200.html', 'dist/200.html')
+      }
+    }
+  ],
   build: {
     outDir: 'dist',
     assetsDir: 'assets'
   }
-}))
+})
